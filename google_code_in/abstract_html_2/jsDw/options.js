ac80985df6fe2630b28e83b72ab14c686e58d365
Dan12 <dangarbage15@yahoo.com>
2013-11-28 10:37:19 -0600

    added google code in folder and abstract html task

---
+++ google_code_in/abstract_html_2/jsDw/options.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	$( "#wrap" ).after( '<div class="option_select"><p class="option option1">Option 1</p><p class="option option2">Option 2</p><p class="option option3">Option 3</p></div>' );
+	$('.option').css({"margin":"2px 20px", "border":"solid gray 2px", "width":"100px", "text-align":"center","padding":"4px", "background-color":"rgb(180,180,180)", "cursor":"pointer"});
+	$('.option_select').css({"position":"absolute","top":"450px", "left":"20px"});
+	$('.option1').css('background-color', 'rgb(230,230,230)');
+	document.getElementById('pagestyle').setAttribute('href', 'stylesheets/option1.css');
+	$('.option1').click(function(){
+		document.getElementById('pagestyle').setAttribute('href', 'stylesheets/option1.css');
+		$('.option1').css('background-color', 'rgb(230,230,230)');
+		$('.option2').css('background-color', 'rgb(180,180,180)');
+		$('.option3').css('background-color', 'rgb(180,180,180)');
+	});
+	$('.option2').click(function(){
+		document.getElementById('pagestyle').setAttribute('href', 'stylesheets/option2.css');
+		$('.option2').css('background-color', 'rgb(230,230,230)');
+		$('.option1').css('background-color', 'rgb(180,180,180)');
+		$('.option3').css('background-color', 'rgb(180,180,180)');
+	});
+	$('.option3').click(function(){
+		document.getElementById('pagestyle').setAttribute('href', 'stylesheets/option3.css');
+		$('.option3').css('background-color', 'rgb(230,230,230)');
+		$('.option1').css('background-color', 'rgb(180,180,180)');
+		$('.option2').css('background-color', 'rgb(180,180,180)');
+	});
+});
