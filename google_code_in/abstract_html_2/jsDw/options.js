2904560d7e51f70314c25460034a8453689dc104
Dan12 <dangarbage15@yahoo.com>
2013-11-29 11:38:52 -0600

    improved styles based off profesional translators

--- google_code_in/abstract_html_2/jsDw/options.js
+++ google_code_in/abstract_html_2/jsDw/options.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$( "#wrap" ).after( '<div class="option_select"><p class="option option1">Option 1</p><p class="option option2">Option 2</p><p class="option option3">Option 3</p></div>' );
-	$('.option').css({"margin":"2px 20px", "border":"solid gray 2px", "width":"100px", "text-align":"center","padding":"4px", "background-color":"rgb(180,180,180)", "cursor":"pointer"});
-	$('.option_select').css({"position":"absolute","top":"450px", "left":"20px"});
+	$('.option').css({"margin":"2px 6px", "border":"solid gray 2px", "width":"100px", "text-align":"center","padding":"4px 10px", "background-color":"rgb(180,180,180)", "cursor":"pointer", "display":"inline"});
+	$('.option_select').css({"position":"absolute","top":"30px", "left":"300px"});
 	$('.option1').css('background-color', 'rgb(230,230,230)');
 	document.getElementById('pagestyle').setAttribute('href', 'stylesheets/option1.css');
 	$('.option1').click(function(){
