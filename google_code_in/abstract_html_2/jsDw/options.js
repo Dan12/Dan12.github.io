e68925add6b148d0abfacbce42128e6f6065e655
Dan12 <dangarbage15@yahoo.com>
2013-12-01 18:40:00 -0600

    updated js option switch and option1+3

--- google_code_in/abstract_html_2/jsDw/options.js
+++ google_code_in/abstract_html_2/jsDw/options.js
@@ -1,25 +1,14 @@
 $(document).ready(function(){
-	$( "#wrap" ).after( '<div class="option_select"><p class="option option1">Option 1</p><p class="option option2">Option 2</p><p class="option option3">Option 3</p></div>' );
-	$('.option').css({"margin":"2px 6px", "border":"solid gray 2px", "width":"100px", "text-align":"center","padding":"4px 10px", "background-color":"rgb(180,180,180)", "cursor":"pointer", "display":"inline"});
-	$('.option_select').css({"position":"absolute","top":"30px", "left":"300px"});
-	$('.option1').css('background-color', 'rgb(230,230,230)');
 	document.getElementById('pagestyle').setAttribute('href', 'stylesheets/option1.css');
-	$('.option1').click(function(){
-		document.getElementById('pagestyle').setAttribute('href', 'stylesheets/option1.css');
-		$('.option1').css('background-color', 'rgb(230,230,230)');
-		$('.option2').css('background-color', 'rgb(180,180,180)');
-		$('.option3').css('background-color', 'rgb(180,180,180)');
-	});
-	$('.option2').click(function(){
-		document.getElementById('pagestyle').setAttribute('href', 'stylesheets/option2.css');
-		$('.option2').css('background-color', 'rgb(230,230,230)');
-		$('.option1').css('background-color', 'rgb(180,180,180)');
-		$('.option3').css('background-color', 'rgb(180,180,180)');
-	});
-	$('.option3').click(function(){
-		document.getElementById('pagestyle').setAttribute('href', 'stylesheets/option3.css');
-		$('.option3').css('background-color', 'rgb(230,230,230)');
-		$('.option1').css('background-color', 'rgb(180,180,180)');
-		$('.option2').css('background-color', 'rgb(180,180,180)');
+	$(document).keypress(function (e) {
+        if (e.keyCode === 49) {
+        	document.getElementById('pagestyle').setAttribute('href', 'stylesheets/option1.css');
+        }
+        if (e.keyCode === 50) {
+        	document.getElementById('pagestyle').setAttribute('href', 'stylesheets/option2.css');
+        }
+        if (e.keyCode === 51) {
+        	document.getElementById('pagestyle').setAttribute('href', 'stylesheets/option3.css');
+        }
 	});
 });
