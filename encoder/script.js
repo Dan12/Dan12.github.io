e6af1b8848cc33920f8e5e8170a48e6e425a9060
Dan12 <dangarbage15@yahoo.com>
2014-02-11 18:32:38 -0600

    added more symbols and increased complexity

--- encoder/script.js
+++ encoder/script.js
@@ -1,7 +1,7 @@
-var alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," ",".",",","'","1","2","3","4","5","6","7","8","9","0","$","!","@","#","%","^","&","*","(",")","+","-","=","_","{","}","[","]","|",";",":","<",">","?","/","\n"];
+var alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," ",".",",","'","1","2","3","4","5","6","7","8","9","0","$","!","@","#","%","^","&","*","(",")","+","-","=","_","{","}","[","]","|",";",":","<",">","?","/","\n","ƒ","š","œ","ÿ","þ","ý","ü","û","ú","ù","ø","ö","õ","ô","ó","ò","ñ","ð","ï","î","í","ì","ë","ê","é","è","ç","æ","å","ä","ã","â","á","à","ß"];
 var letter=1;
 var letters=[];
-var alphabet2 = new Array(66);
+var alphabet2 = new Array(101);
 var alpha1=0;
 var alpha2=0;
 var alpha3=0;
@@ -14,14 +14,14 @@ $(document).ready(function () {
         a=a.toLowerCase();
         letters=a.split('');
         letter=1;
-        var encode1=Math.floor(Math.random() * 66 + 1);
-        var encode2=Math.floor(Math.random() * 66 + 1);
-        var encode3=Math.floor(Math.random() * 66 + 1);
-        var encode4=Math.floor(Math.random() * 66 + 1);
-        alpha1=Math.floor(Math.random() * 66 + 1);
-        alpha2=Math.floor(Math.random() * 66 + 1);
-        alpha3=Math.floor(Math.random() * 66 + 1);
-        alpha4=Math.floor(Math.random() * 66 + 1);
+        var encode1=Math.floor(Math.random() * 101 + 1);
+        var encode2=Math.floor(Math.random() * 101 + 1);
+        var encode3=Math.floor(Math.random() * 101 + 1);
+        var encode4=Math.floor(Math.random() * 101 + 1);
+        alpha1=Math.floor(Math.random() * 101 + 1);
+        alpha2=Math.floor(Math.random() * 101 + 1);
+        alpha3=Math.floor(Math.random() * 101 + 1);
+        alpha4=Math.floor(Math.random() * 101 + 1);
         new_alpha();
         for(var x = 0; x<letters.length; x++){
             var ind=0;
@@ -41,8 +41,8 @@ $(document).ready(function () {
             if (letter==4){
                 ind+=encode4;
             }
-            if(ind>=66){
-                ind-=66;
+            if(ind>=101){
+                ind-=101;
             }
             letters[x]=alphabet2[ind];
             letter++;
@@ -88,7 +88,7 @@ $(document).ready(function () {
                 ind-=encode4;
             }
             if(ind<0){
-                ind+=66;
+                ind+=101;
             }
             letters[x]=alphabet2[ind];
             letter++;
@@ -104,7 +104,7 @@ $(document).ready(function () {
 
 function new_alpha() {
     letter=1;
-    alphabet2 = new Array(66);
+    alphabet2 = new Array(101);
     for (var i = 0; i<alphabet.length; i++){
         var ind = 0;
         if (letter==1){
@@ -120,8 +120,8 @@ function new_alpha() {
             ind+=alpha4;
         }
         while(true){
-            if (ind>=66){
-                ind-=66;
+            if (ind>=101){
+                ind-=101;
             }
             if (alphabet2[ind]===undefined){
                 alphabet2[ind]=alphabet[i];
