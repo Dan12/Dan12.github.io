0a35359a2df5db559a39ba661e47e561e9802b0d
Dan12 <dangarbage15@yahoo.com>
2014-02-27 17:35:38 -0600

    added something to try to detect bug

--- script.js
+++ script.js
@@ -10,7 +10,9 @@ $(document).ready(function(){
 function resize(){
 	var h = $('.text_box').height()+640;
 	$('.sidebar').css("height",h);
-	if (h<document.height()){
+	alert(h+" "+window.height());
+	if (h<window.height()){
 		$('.sidebar').css("height","100%");
+		alert("larger");
 	}
 }
