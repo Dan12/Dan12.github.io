f1c28ede0dafc453be2b76bc55585b8566db0c62
Dan12 <dangarbage15@yahoo.com>
2014-02-27 19:56:28 -0600

    added and updated
    added google code in redirect page
    added google code in and image slider to more we bites
    added loading gif to image slider
    modified image slider js for better image load times
    added image slider .zip file download

--- img_slider/script.js
+++ img_slider/script.js
@@ -11,6 +11,11 @@ var img_width = 192;
 //finally set img_scale to the amount you want to scale your image.
 var img_scale = 3;
 //set img_scale to 1 if you want the original dimensions and set img_scale to a number between 1 and 0 is you want to make your image smaller.
+//you can also change the effect duration (effect_dur) and the time between each image is displayed (transition_time). 
+//Warning, will not work if effect_dur is greater that 100 millisecods less than transition_time.
+//Times are in milliseconds 1 second = 1000 milliseconds
+var effect_dur = 1600;
+var transition_time = 5000;
 var i;
 var img_num;
 var prim_img_cont = 1;
@@ -19,11 +24,10 @@ var seconds = 0;
 var prev_index = 1;
 var interval1;
 var going = false;
-var effect_dur = 1600;
-var transition_time = 5000;
 $(document).ready(function () {
-	if (img_scale<0 || img_width<0 || img_height<0 || img<0){
-		alert('The values you entered will not produce and image. Resetting unacceptable values to default values');
+	$('.container').css({'background':'white url("ajax-loader.gif")','background-position':'center','z-index':'20','width':(img_width*img_scale)+'px','height':(img_height*img_scale)+'px','background-repeat':'no-repeat'});
+	if (img_scale<0 || img_width<0 || img_height<0 || img<0 || effect_dur > transition_time-100){
+		alert('The values you entered will not produce and image or will not effectivley transition. Resetting unacceptable values to default values');
 		if (img_scale<0){
 			img_scale = 1;
 		}
@@ -36,30 +40,35 @@ $(document).ready(function () {
 		if (img_height<0){
 			img_height = 192;
 		}
+		if (effect_dur>transition_time-100){
+			effect_dur = 1600;
+			transition_time = 5000;
+		}
 	}
 	for (i = 1; i <= img; i++){
-		$('.img_container_1').css('background-image','url(img'+i+'.jpeg)');	
 		img_num = i;
+		$('.container').append('<div class="img_container_'+img_num+' img_container"></div>');
+		$('.img_container_'+img_num).css({'background-image':'url(img'+img_num+'.jpeg)','z-index':'-10'});
 	}
+	$('.img_container_1').css('z-index','10');
 	for (i = 1; i <= img; i++){
 		$('.slider_button_holder').append('<div class="slider_button slider_button_'+i+'" value="'+i+'"></div>');
 	}
 	$('.slider_button_holder').css({'width':48*img+'px','top':(img_height*img_scale+80)+'px','margin-left':((img_width*img_scale-48*img)/2)+'px'});
 	$('.img_container').css({'width':(img_width*img_scale)+'px','height':(img_height*img_scale)+'px','background-size':(img_width*img_scale)+'px '+(img_height*img_scale)+'px'});
-	$('.container').css({'width':(img_width*img_scale)+'px','height':(img_height*img_scale)+'px'})
-	img_num = 1;
-	$('.img_container_1').css('background-image','url(img'+img_num+'.jpeg)');	
+	img_num = 1;	
 	$('.slider_button_'+img_num+'').css('background-position','0px 10px');
-	start();
-	$('.slider_button_'+img_num+'').ready(function(){
-		$('.slider_button').click(function(){
-			if (!going) {
-				clearInterval(interval1);
-				img_num = parseInt($(this).attr("value"));
-				new_img();
-				start();
-			}
-		});
+	$('.img_container_'+img).ready(function(){
+		$('.container').css({'z-index':'-1','background':'rgba(0,0,0,0)'});
+		start();
+	});
+	$('.slider_button').click(function(){
+		if (!going) {
+			clearInterval(interval1);
+			img_num = parseInt($(this).attr("value"));
+			new_img();
+			start();
+		}
 	});
 });
 
@@ -78,14 +87,13 @@ function start(){
 function new_img(){
 	$('.slider_button').css('background-position','0px 0px');
 	$('.slider_button_'+img_num+'').css('background-position','0px 10px');
-	$('.img_container_'+sec_img_cont+'').css({'background-image':'url(img'+img_num+'.jpeg)'});
 	going = true;
+	sec_img_cont = img_num;
+	$('.img_container_'+sec_img_cont).css('z-index','5');
 	$('.img_container_'+prim_img_cont+'').animate({opacity: 1/effect_dur},effect_dur,"linear",function(){
 		$('.img_container_'+prim_img_cont+'').css({'z-index':'-10','opacity':'1'});
 		$('.img_container_'+sec_img_cont+'').css({'z-index':'10','opacity':'1'});
-		var img_cont_holder = prim_img_cont;
 		prim_img_cont = sec_img_cont;
-		sec_img_cont = img_cont_holder;
 		going = false;
 	});
 }
