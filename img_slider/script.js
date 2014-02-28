d13942023dd8bd563c58ef65c2e275102e659c06
Dan12 <dangarbage15@yahoo.com>
2014-02-27 18:37:02 -0600

    improved css and added modifying instructions

--- img_slider/script.js
+++ img_slider/script.js
@@ -1,4 +1,16 @@
+//This image slider can be very easily manipulated to your liking. It is recomended to have images of the same dimensions.
+//First, you have to make sure that all of your images are name with img and then their number and then .jpeg, 
+//for example, the first image should be called "img1.jpeg", the second "img2.jpeg" and so on.
+//look at the pictures that come with this file if you do not understand.
+//Now, set the variable img equal to the amount of images you have
 var img = 8;
+//Now, set the variable img_height to the standard height of your image
+var img_height = 120;
+//and now set img_width to the standard width of your image
+var img_width = 192;
+//finally set img_scale to the amount you want to scale your image.
+var img_scale = -1;
+//set img_scale to 1 if you want the original dimensions and set img_scale to a number between 1 and 0 is you want to make your image smaller.
 var i;
 var img_num;
 var prim_img_cont = 1;
@@ -9,10 +21,22 @@ var interval1;
 var going = false;
 var effect_dur = 1600;
 var transition_time = 5000;
-var img_height = 120;
-var img_width = 192;
-var img_scale = 5;
 $(document).ready(function () {
+	if (img_scale<0 || img_width<0 || img_height<0 || img<0){
+		alert('The values you entered will not produce and image. Resetting unacceptable values to default values');
+		if (img_scale<0){
+			img_scale = 1;
+		}
+		if (img<0){
+			img = 1;
+		}
+		if (img_width<0){
+			img_width = 120;
+		}
+		if (img_height<0){
+			img_height = 192;
+		}
+	}
 	for (i = 1; i <= img; i++){
 		$('.img_container_1').css('background-image','url(img'+i+'.jpeg)');	
 		img_num = i;
@@ -20,21 +44,22 @@ $(document).ready(function () {
 	for (i = 1; i <= img; i++){
 		$('.slider_button_holder').append('<div class="slider_button slider_button_'+i+'" value="'+i+'"></div>');
 	}
-	$('.slider_button_holder').css({'width':48*img+'px','top':'-'+(img_height*img_scale-8)+'px'});
-	$('.img_container').css({'width':(img_width*img_scale)+'px','height':(img_height*img_scale)+'px'});
-	$('.img_container_1').css('background-size',(img_width*img_scale)+'px '+(img_height*img_scale)+'px');
-	$('.img_container_2').css({'background-size':(img_width*img_scale)+'px '+(img_height*img_scale)+'px','top':'-'+(img_height*img_scale+2)+'px'});
+	$('.slider_button_holder').css({'width':48*img+'px','top':(img_height*img_scale+80)+'px','margin-left':((img_width*img_scale-48*img)/2)+'px'});
+	$('.img_container').css({'width':(img_width*img_scale)+'px','height':(img_height*img_scale)+'px','background-size':(img_width*img_scale)+'px '+(img_height*img_scale)+'px'});
+	$('.container').css({'width':(img_width*img_scale)+'px','height':(img_height*img_scale)+'px'})
 	img_num = 1;
 	$('.img_container_1').css('background-image','url(img'+img_num+'.jpeg)');	
 	$('.slider_button_'+img_num+'').css('background-position','0px 10px');
 	start();
-	$('.slider_button').click(function(){
-		if (!going) {
-			clearInterval(interval1);
-			img_num = parseInt($(this).attr("value"));
-			new_img();
-			start();
-		}
+	$('.slider_button_'+img_num+'').ready(function(){
+		$('.slider_button').click(function(){
+			if (!going) {
+				clearInterval(interval1);
+				img_num = parseInt($(this).attr("value"));
+				new_img();
+				start();
+			}
+		});
 	});
 });
 
