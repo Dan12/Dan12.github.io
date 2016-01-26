a93a31bbf41b9d470b9c66eccfdcf67981e96f54
Dan W <dantech88@yahoo.com>
2016-01-26 03:28:51 +0000

    grammer fixing and js fixing

--- script.js
+++ script.js
@@ -7,9 +7,14 @@ $(document).ready(function(){
     
     //TODO: add function to reformat for web pages
 
+    if(window.location.hash) {
+        console.log(parseInt(window.location.hash.substr(1,window.location.hash.length)))
+        CURRENTTAB = parseInt(window.location.hash.substr(1,window.location.hash.length));
+    }
+
     PAGEWIDTH = parseInt($(".container").css("max-width"));
 
-    if($(".project_image").css("display") == "none"){
+    if($("#mobile_check_div").css("display") == "none"){
       ISMOBILE = true;
       reformatMobile();
     }
@@ -19,20 +24,24 @@ $(document).ready(function(){
     loadPage();
     
     $(window).resize(function(){
-      if($(".project_image").css("display") == "none")
-        ISMOBILE = true;
-      else
-        ISMOBILE = false;
-      console.log(ISMOBILE);
-      if(ISMOBILE)
-        reformatMobile();
-      else
-        reformatRegular(); 
+        if($("#mobile_check_div").css("display") == "none")
+            ISMOBILE = true;
+        else
+            ISMOBILE = false;
+        console.log(ISMOBILE);
+        if(ISMOBILE)
+            reformatMobile();
+        else
+            reformatRegular(); 
     });
     
 });
 
 function reformatMobile(){
+    $("[tab_num='1']").css("margin","0");
+    $("[tab_num='2']").css("margin","0");
+    $("[tab_num='3']").css("margin","0");
+    $("[tab_num='4']").css("margin","0");
     PAGEWIDTH = parseInt($(".nav_tabs").width())-40;
     var widthSum = 0;
     for(var i = 1; i <= TABNUM/2; i++){
@@ -41,8 +50,8 @@ function reformatMobile(){
     var padding = (PAGEWIDTH-widthSum)/(TABNUM/2);
     padding-=2;
     if(padding > 0){
-      $("[tab_num='1']").css("margin","0 "+padding+"px 0 20px");
-      $("[tab_num='2']").css("margin","0 20px 0 "+padding+"px");
+        $("[tab_num='1']").css("margin","0 "+padding+"px 0 20px");
+        $("[tab_num='2']").css("margin","0 20px 0 "+padding+"px");
     }
     widthSum = 0;
     for(var i = TABNUM/2+1; i <= TABNUM; i++){
@@ -51,22 +60,24 @@ function reformatMobile(){
     padding = (PAGEWIDTH-widthSum)/(TABNUM/2);
     padding-=2;
     if(padding > 0){
-      $("[tab_num='3']").css("margin","0 "+padding+"px 0 20px");
-      $("[tab_num='4']").css("margin","0 20px 0 "+padding+"px");
+        $("[tab_num='3']").css("margin","0 "+padding+"px 0 20px");
+        $("[tab_num='4']").css("margin","0 20px 0 "+padding+"px");
     }
 }
 
 function loadPage(){
     $(".tab_"+CURRENTTAB).slideDown(300);
+    $("[tab_num='"+CURRENTTAB+"']").addClass("nav_selected");
     
     $(".nav_tab").click(function(){
-      var newTabNum = $(this).attr("tab_num");
-      $("[tab_num='"+CURRENTTAB+"']").removeClass("nav_selected");
-      $(".tab_"+CURRENTTAB).slideUp(300, function(){
-         CURRENTTAB =  newTabNum;
-         $("[tab_num='"+CURRENTTAB+"']").addClass("nav_selected");
-         $(".tab_"+CURRENTTAB).slideDown(300);
-      });
+        var newTabNum = $(this).attr("tab_num");
+        $("[tab_num='"+CURRENTTAB+"']").removeClass("nav_selected");
+        $(".tab_"+CURRENTTAB).slideUp(300, function(){
+            CURRENTTAB =  newTabNum;
+            $("[tab_num='"+CURRENTTAB+"']").addClass("nav_selected");
+            $(".tab_"+CURRENTTAB).slideDown(300);
+            window.location.hash = CURRENTTAB;
+        });
     });
 }
 
