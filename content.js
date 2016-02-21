520809489fb9f94700b0b2c6cc7c2fa81dba7ff5
Dan W <dantech88@yahoo.com>
2016-02-21 18:20:45 +0000

    started new framework

---
+++ content.js
@@ -0,0 +1,25 @@
+var content = {
+    "About":[
+        "I am currently a senior in high school and enjoy creating and building a wide range of projects. I focus mostly on electronics and programming projects. My favorite hobby is coding, and I have learned many different languages and have found many different applications for my knowledge. I consistently take programming related summer courses, mostly through Northwestern's <a,http://www.ctd.northwestern.edu/,inline_a_tag>CTD</a>, and I have learned how to make web pages with HTML, CSS, and JavaScript and how to create database-backed applications using the Ruby on Rails framework."
+        ,"I have also achieved significant proficiency in the Java programming language over many years of practice and application and have also gained a large amount of knowledge about Arduino programming and electronics. I also recently got a Raspberry Pi and have been experimenting with its GPIO capabilities. I recently took an online course through Coursera called <a,https://www.coursera.org/learn/machine-learning,inline_a_tag>Machine Learning</a>, which was very interesting and has influenced me to study Machine Learning and Artificial Intelligence more in the future. This interest in Machine Learning influenced me to take another online course titled <a,https://www.coursera.org/learn/synapses,inline_a_tag>Synapses, Neurons, and the Brain</a>. Apart from programming, I also have played soccer for over a decade for various teams and have been doing Taekwondo for several years now."
+        ],
+    "Websites":[
+        // header, content, image
+        [
+            "https://lit-beyond-4450.herokuapp.com/,Game Design Host",
+            "This is a hosting service for Unity games that I created for my game design class. It is built using the Ruby on Rails framework. The GitHub repository for this project can be found <a,https://github.com/Dan12/GDHost,inline_a_tag>here</a>.",
+            "https://lit-beyond-4450.herokuapp.com/,images/gdhost.png"
+        ],
+        [
+            "",
+            "",
+            ""
+        ]
+    ],
+    "Other Projects":{
+        
+    },
+    "Competitions":{
+        
+    }
+}
